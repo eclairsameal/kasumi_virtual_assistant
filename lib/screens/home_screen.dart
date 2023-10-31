f70d36614cb079e62ae96f92bee173847fc192c8
Huang Peiwen <peiwen.huang@bric.jp>
2023-10-31 23:35:38 +0900

    add button for voice input and text field for text input with animated button

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,12 +9,92 @@ class HomeScreen extends StatefulWidget
 
 class _HomeScreenState extends State<HomeScreen>
 {
+  TextEditingController userInputTextEditingController = TextEditingController();
   @override
   Widget build(BuildContext context)
   {
     return Scaffold(
       appBar: AppBar(
-        title: Text("hello world"),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.purpleAccent.shade100,
+                Colors.deepPurple,
+              ]
+            )
+          ),
+        ),
+        title: Image.asset(
+          "images/logo.png",
+          width: 140,
+        ),
+        titleSpacing: 10,
+        elevation: 14,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Center(
+                child: InkWell(
+                  onTap: (){
+
+                  },
+                    child: Image.asset(
+                      "images/assistant_icon.png",
+                      height: 300,
+                      width: 300,
+                    ),
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              // text field with a button
+              Row(
+                children: [
+                  // text field
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 4.0),
+                      child: TextField(
+                        controller: userInputTextEditingController,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: "How can i help you?",
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  InkWell(
+                    onTap:(){
+                      print("send user input");  // Test button event
+                    },
+                    child: AnimatedContainer(
+                      padding: const EdgeInsets.all(15),
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.rectangle,
+                        color: Colors.deepPurpleAccent,
+                      ),
+                      duration: const Duration(
+                        milliseconds: 1000,
+                      ),
+                      curve: Curves.bounceInOut,
+                      child: const Icon(
+                        Icons.send,
+                        color: Colors.white,
+                        size: 30,
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
